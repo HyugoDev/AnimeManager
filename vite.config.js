c4977b94879f44ddeeceb0393268ba14279b8136
HyugoDev <h.mejias.ugalde53@gmail.com>
2022-09-10 16:19:31 -0600

    se poner url de la api

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://api-torii.up.railway.app/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
